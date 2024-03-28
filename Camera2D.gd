b161f19403fee0a52020dd97355ca881c1dea23c
nawarian <nickolas@phpsp.org.br>
2024-03-28 02:47:22 +0100

    fix mobile screen aspect ratio

--- Camera2D.gd
+++ Camera2D.gd
@@ -7,15 +7,3 @@ func _ready():
 
 func _process(delta):
 	position = player.global_position
-
-func _on_LeftTouch_pressed():
-	Input.action_press("move_left")
-
-func _on_LeftTouch_released():
-	Input.action_release("move_left")
-
-func _on_RightTouch_pressed():
-	Input.action_press("move_right")
-
-func _on_RightTouch_released():
-	Input.action_release("move_right")
--- HUD.gd
+++ HUD.gd
@@ -6,5 +6,20 @@ func _process(delta):
 	$FlashlightToggleOff.visible = !player.is_flashlight_on()
 	$FlashlightToggleOn.visible = player.is_flashlight_on()
 
-func _on_FlashlightToggle_pressed():
+func _on_FlashlightToggleOff_pressed():
 	player.toggle_flashlight()
+
+func _on_FlashlightToggleOn_pressed():
+	player.toggle_flashlight()
+
+func _on_LeftTouch_pressed():
+	Input.action_press("move_left")
+
+func _on_LeftTouch_released():
+	Input.action_release("move_left")
+
+func _on_RightTouch_pressed():
+	Input.action_press("move_right")
+
+func _on_RightTouch_released():
+	Input.action_release("move_right")
